81502dc37c1b9a46f0a603c0a7a620b6676396fb
Akshay Ganesh <akshayiyerg@gmail.com>
2025-01-15 16:20:58 -0500

    Inital Commit, Project Assets added, Basic layout of view is added

---
+++ CurrencyConvertor_SWIFTUI/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  CurrencyConvertor_SWIFTUI
+//
+//  Created by Akshay  on 2025-01-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            // Background image
+            
+            VStack {
+                // Prancing pony image
+                
+                // currency exchange text
+                
+                // Conversion section
+                HStack {
+                    // Left Conversion section
+                    VStack {
+                        // Currency
+                        HStack {
+                            // Currency Image
+                            
+                            // Currency text
+                        }
+                        
+                        // Text field
+                    }
+                    
+                    // Equal Sign
+                    
+                    // Right Conversion section
+                    VStack {
+                        // Currency
+                        HStack {
+                            // Currency text
+                            
+                            // Currency image
+                        }
+                        
+                        // Text field
+                    }
+                }
+                
+                // info button
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CurrencyConvertor_SWIFTUI/CurrencyConvertor_SWIFTUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConvertor_SWIFTUIApp.swift
+//  CurrencyConvertor_SWIFTUI
+//
+//  Created by Akshay  on 2025-01-14.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConvertor_SWIFTUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

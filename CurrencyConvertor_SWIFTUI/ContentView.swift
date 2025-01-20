25e0ecb23151a66cf30cd6bb12bfa7a84116449d
Akshay Ganesh <akshayiyerg@gmail.com>
2025-01-19 22:20:30 -0500

    Info button is added and Screen design is complete

--- CurrencyConvertor_SWIFTUI/ContentView.swift
+++ CurrencyConvertor_SWIFTUI/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showExchangeInfo: Bool = false
+    @State var leftAmount = ""
+    @State var rightAmount = ""
+    
     var body: some View {
         ZStack {
             // Background image
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
             
             VStack {
                 // Prancing pony image
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
                 
                 // currency exchange text
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundColor(Color.white)
+                
                 
                 // Conversion section
                 HStack {
@@ -24,29 +39,71 @@ struct ContentView: View {
                         // Currency
                         HStack {
                             // Currency Image
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
                             
                             // Currency text
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundColor(Color.white)
+                            
                         }
+                        .padding(.bottom, -5)
                         
                         // Text field
+                        TextField("Amount", text: $leftAmount)
+                            .textFieldStyle(.roundedBorder)
                     }
                     
                     // Equal Sign
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
                     
                     // Right Conversion section
                     VStack {
                         // Currency
                         HStack {
                             // Currency text
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundColor(Color.white)
                             
                             // Currency image
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
                         }
+                        .padding(.bottom, -5)
                         
                         // Text field
+                        TextField("Amount", text: $leftAmount)
+                            .textFieldStyle(.roundedBorder)
+                            .multilineTextAlignment(.trailing)
                     }
                 }
+                .padding()
+                .background(.black.opacity(0.5))
+                .clipShape(.capsule)
+                
+                Spacer()
                 
                 // info button
+                HStack {
+                    Spacer()
+                    Button {
+                        showExchangeInfo.toggle()
+                    } label: {
+                        Image(systemName: "info.circle.fill")
+                            .font(.largeTitle)
+                            .foregroundStyle(.white)
+                    }
+                    .padding(.trailing)
+                }
             }
         }
     }

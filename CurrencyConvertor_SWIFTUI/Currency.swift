71c8087023cf5056d73a5ac86f2597f409b8e689
Akshay Ganesh <akshayiyerg@gmail.com>
2025-01-24 21:14:25 -0500

    Model created for enum and set the values for each

---
+++ CurrencyConvertor_SWIFTUI/Currency.swift
@@ -0,0 +1,15 @@
+//
+//  Currency.swift
+//  CurrencyConvertor_SWIFTUI
+//
+//  Created by Akshay  on 2025-01-24.
+//
+
+
+enum Currency: Double {
+    case copperPenny = 6400
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPiece = 1
+}
